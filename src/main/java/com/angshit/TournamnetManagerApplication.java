8e3cadcdee3b2d609c0af33c21bc90ca7e9d308c
Angshit <angshitghosh@gmail.com>
2020-07-25 13:41:37 +0530

    Committing Initial Files

---
+++ src/main/java/com/angshit/TournamnetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.angshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamnetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamnetManagerApplication.class, args);
+	}
+
+}
